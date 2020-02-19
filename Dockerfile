ff5a3ec1e22d4e23a7ab7ec186481fe18f5909ba
jaco <jaco@mediadistillery.com>
2020-02-19 13:45:06 +0100

    dockerfile fix

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM python:3.7
-COPY pychyderm-exporter.py /
-RUN pip install prometheus_client pachypy python-pachyderm
+COPY src/pychyderm-exporter.py /
+RUN pip install prometheus_client python-pachyderm
 EXPOSE 9426
-CMD [ "python", "pychyderm-exporter.py","-i", "pachd", "-p", "9426" ]
+CMD [ "python", "pychyderm-exporter.py","--host", "pachd", "--port", "30650" ]
---
+++ src/pychyderm-exporter.py
@@ -0,0 +1,60 @@
+import argparse
+from prometheus_client import start_http_server, Gauge
+import time
+import python_pachyderm
+
+gauges = dict()
+succescounters = dict()
+failcounters = dict()
+
+
+def update_gauges(client):
+    global gauges
+    pipelines = client.list_pipeline().pipeline_info
+    for pipeline in pipelines:
+        name = str(pipeline.pipeline.name).replace("-", "_")
+        if name in gauges:
+            print("{} updated".format(name))
+            gauges[name].set(pipeline.last_job_state)
+            # if pipeline.job_counts[3] > succescounters[name]:
+            #     gauges[name].set(0)
+            #     succescounters[name] = pipeline.job_counts[3]
+            # if pipeline.job_counts[2] > failcounters[name]:
+            #     gauges[name].set(1)
+            #     succescounters[name] = pipeline.job_counts[2]
+
+
+def main():
+    global gauges
+    args = parse_args()
+    port = args.port
+    host = args.host
+
+    pachyclient = python_pachyderm.Client(host=host, port=port)
+    start_http_server(port=9426)
+    pipelines = pachyclient.list_pipeline().pipeline_info
+    for pipeline in pipelines:
+        name = str(pipeline.pipeline.name).replace("-", "_")
+
+        last_job_state = pipeline.last_job_state
+        g = Gauge("{}_last_job".format(name),
+                  "Last Job status of pipeline {}".format(name))
+        gauges[name] = g
+        succescounters[name] = pipeline.job_counts[3]
+        failcounters[name] = pipeline.job_counts[2]
+    while True:
+        update_gauges(pachyclient)
+        time.sleep(60)
+
+
+def parse_args(args=None):
+    parser = argparse.ArgumentParser(description='Prometheus exporter for PachyDerm metrics')
+    parser.add_argument("-i", "--host", type=str, default="pachd", help="Host of pachd")
+    parser.add_argument("-p", "--port", type=int, default=30650,
+                        help="port number")
+
+    return parser.parse_args(args=args)
+
+
+if __name__ == '__main__':
+    main()
